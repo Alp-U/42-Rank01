2ba543e4588568826cdf2a7e745786645799a80b
Alp Utku <autku@c3r4p5.42vienna.com>
2024-04-27 16:50:27 +0200

    changes

---
+++ printf/ft_print_ptr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_ptr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: autku <autku@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 21:30:11 by autku             #+#    #+#             */
+/*   Updated: 2024/04/24 22:21:17 by autku            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	print_ptr(long n, int base)
+{
+	int		count;
+	char	*hexd;
+
+	hexd = "0123456789abcdef";
+	if (n < base)
+		return (print_char(hexd[n]));
+	count = print_ptr(n / base, base);
+	return (count + print_ptr(n % base, base));
+}
